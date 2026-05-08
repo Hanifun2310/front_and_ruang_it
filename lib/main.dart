cb4d9a1df751904966dce081a09f411d5875d6ec
Muhammad Habiburrahman Al-Arif <habibmas2007@gmail.com>
2026-05-08 18:36:39 +0700

    Login Session

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'app/routes/app_pages.dart';
+import 'app/routes/app_routes.dart';
 import 'app/data/services/auth_service.dart';
 
 void main() async {
@@ -11,13 +12,13 @@ void main() async {
   await GetStorage.init();
   
   // 2. Registrasi AuthService secara global
-  Get.put(AuthService());
+  final authService = Get.put(AuthService());
 
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Ruang IT",
-      initialRoute: AppPages.INITIAL,
+      initialRoute: authService.isLoggedIn.value ? Routes.DASHBOARD : AppPages.INITIAL,
       getPages: AppPages.routes,
       theme: ThemeData(
         useMaterial3: true,
